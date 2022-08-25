0b22b1f857674d39378f17f8886e4909f4041926
afroza732 <afroza1996>
2022-08-25 17:06:49 +0600

    Sanctum Auth With Vue 3

--- resources/js/layouts/app.vue
+++ resources/js/layouts/app.vue
@@ -1,9 +1,28 @@
 <template>
     <div>
-       <router-link :to="{name:'Home'}">Home</router-link> |
-       <router-link :to="{name:'Login'}">Login</router-link> |
-       <router-link :to="{name:'Register'}">Register</router-link> |
-
+      
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <a class="navbar-brand" href="#">Navbar</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNavDropdown">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                         <router-link :to="{name:'Home'}" class="nav-link">Home</router-link>
+                    </li>
+                    <li class="nav-item">
+                         <router-link :to="{name:'Login'}" class="nav-link">Login</router-link>
+                    </li>
+                    <li class="nav-item">
+                         <router-link :to="{name:'Register'}" class="nav-link">Register</router-link>
+                    </li>
+                     <li class="nav-item">
+                         <router-link :to="{name:'Dashboard'}" class="nav-link">Dashboard</router-link>
+                    </li>
+                </ul>
+            </div>
+        </nav>
        <router-view></router-view>
     </div>
 </template>
